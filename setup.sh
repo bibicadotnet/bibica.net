3385bf18e3dba7bdc144c45e7f46daba323956b0
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-28 22:41:00 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,20 +2,12 @@
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Webinoly-Optimization/master/webinoly74.sh -O webinoly_mod.sh && sudo chmod +x webinoly_mod.sh && sudo ./webinoly_mod.sh
 sudo site bibica.net -wp
 sudo httpauth bibica.net -wp-admin=off
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+mkdir -p /root/ssl
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/bibica.net.pem -O /root/ssl
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/bibica.net.key -O /root/ssl
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/bibica.net -O /etc/nginx/sites-available/bibica.net
+sudo site api.bibica.net -proxy=[https://i0.wp.com/cdn.bibica.net/wp-content/uploads/] -dedicated-reverse-proxy=simple
+nginx -t
+service nginx reload
+cd /var/www/bibica.net/htdocs
+rm -rf *
