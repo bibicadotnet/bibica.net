64c7faafbaaa9a0a3c20afecd27b3319ecc9ab6a
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-28 22:21:48 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Webinoly-Optimization/master/webinoly74.sh -O webinoly_mod.sh && sudo chmod +x webinoly_mod.sh && sudo ./webinoly_mod.sh
+sudo site bibica.net -wp
+sudo httpauth bibica.net -wp-admin=off
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
