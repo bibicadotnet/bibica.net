581103458d15909d1a0a4abee39d0576cd898c61
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-29 00:12:12 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Webinoly-Optimization/master/webinoly74.sh -O webinoly_mod.sh && sudo chmod +x webinoly_mod.sh && sudo ./webinoly_mod.sh
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Webinoly-Optimization/master/webinoly_beta.sh -O webinoly_mod.sh && sudo chmod +x webinoly_mod.sh && sudo ./webinoly_mod.sh
 sudo site bibica.net -wp
 sudo httpauth bibica.net -wp-admin=off
 mkdir -p /root/ssl
