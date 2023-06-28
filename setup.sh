924394b40e7b7c26bb93437492224c80267196c7
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-28 23:11:25 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/bibica.net.key -O /root/ssl/bibica.net.key
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/bibica.net -O /etc/nginx/sites-available/bibica.net
 sudo site api.bibica.net -proxy=[https://i0.wp.com/cdn.bibica.net/wp-content/uploads/] -dedicated-reverse-proxy=simple
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/api.bibica.net -O //etc/nginx/sites-available/api.bibica.net
 nginx -t
 service nginx reload
 cd /var/www/bibica.net/htdocs
