26766d00ff9d5131d9d6cb796000540cffff59bf
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-29 23:10:25 +0700

    Update setup_remote_proxy.sh

--- setup_remote_proxy.sh
+++ setup_remote_proxy.sh
@@ -11,6 +11,8 @@ sudo apt install unzip -y
 sudo apt install htop -y
 sudo apt install screen -y
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/webinoly/master/weby -O weby && sudo chmod +x weby && sudo ./weby -clean
+sudo rm /opt/webinoly/webinoly.conf
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Webinoly-Optimization/master/webinoly_php74.conf -O /opt/webinoly/webinoly.conf
 sudo stack -mysql
 sudo stack -nginx
 # setup proxy api.bibica.net
