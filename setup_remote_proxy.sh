d8d7cef3341d7bed4da0f35fcab9dc55a8e0f4f6
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-29 22:53:22 +0700

    Update and rename setup_database-remote.sh to setup_remote_proxy.sh

---
+++ setup_remote_proxy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+locale-gen en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+sudo apt update -y
+sudo apt install zip -y
+sudo apt install unzip -y
+sudo apt install htop -y
+sudo apt install screen -y
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/webinoly/master/weby -O weby && sudo chmod +x weby && sudo ./weby -clean
+sudo stack -mysql
+sudo stack -nginx
+# setup proxy api.bibica.net
+sudo site api.bibica.net -proxy=[https://i0.wp.com/cdn.bibica.net/wp-content/uploads/] -dedicated-reverse-proxy=simple
+# setup ssl
+mkdir -p /root/ssl
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/bibica.net.pem -O /root/ssl/bibica.net.pem
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/bibica.net.key -O /root/ssl/bibica.net.key
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/bibica.net.crt -O /root/ssl/bibica.net.crt
+# setup ssl for bibica.net and api.bibica.net
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/api.bibica.net -O //etc/nginx/sites-available/api.bibica.net
+# nginx reload
+nginx -t
+sudo service nginx reload
+# mariadb config
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/my.cnf -O /etc/mysql/my.cnf
+sudo service mysql restart
+sudo webinoly -mysql-public-access=on -create-master-user=[masteruser,abcdefgh]
+sudo site -mysql
