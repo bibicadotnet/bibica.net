6f900e31b30fed7d462a579bcfdf5c68ebe47eb5
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-29 23:52:22 +0700

    Update setup_webserver.sh

--- setup_webserver.sh
+++ setup_webserver.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# setup Webinoly php 7.4
+# setup Webinoly
+sudo apt remove iptables-persistent -y
+sudo ufw disable
+sudo iptables -F
 locale-gen en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
@@ -12,46 +15,17 @@ sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/
 sudo rm /opt/webinoly/webinoly.conf
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Webinoly-Optimization/master/webinoly_php74.conf -O /opt/webinoly/webinoly.conf
 sudo stack -nginx
-
-
-
-
-sudo stack -lemp -build=light
-sudo apt remove iptables-persistent -y
-sudo ufw disable
-sudo iptables -F
+sudo stack -php
 sudo apt update && sudo apt upgrade -y
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Webinoly-Optimization/master/php.ini -O /etc/php/7.4/fpm/php.ini
 sudo service php7.4-fpm restart
-sudo webinoly -verify
-sudo webinoly -info
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Webinoly-Optimization/master/webinoly74.sh -O webinoly_mod.sh && sudo chmod +x webinoly_mod.sh && sudo ./webinoly_mod.sh
 # setup wp-cli
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 php wp-cli.phar --info
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
 # setup rclone
-sudo -v ; curl https://rclone.org/install.sh | sudo bash
+curl https://rclone.org/install.sh | sudo bash
 # setup crontab cho wp_cron and simply-static
 mkdir -p /var/www/bibica.net/static-files-temp
 chmod 777 /var/www/bibica.net/static-files-temp
@@ -60,25 +34,17 @@ echo "0 3 * * * /usr/local/bin/wp --path='/var/www/bibica.net/htdocs' simply-sta
 echo "*/1 * * * * curl https://bibica.net/wp-cron.php?doing_wp_cron > /dev/null 2>&1" >> simply-static
 crontab simply-static
 # setup wordpress bibica.net and off httpauth
-sudo site bibica.net -wp
-sudo httpauth bibica.net -wp-admin=off
-# setup proxy api.bibica.net
-sudo site api.bibica.net -proxy=[https://i0.wp.com/cdn.bibica.net/wp-content/uploads/] -dedicated-reverse-proxy=simple
+sudo site bibica.net -php
+
 # setup ssl
 mkdir -p /root/ssl
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/bibica.net.pem -O /root/ssl/bibica.net.pem
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/bibica.net.key -O /root/ssl/bibica.net.key
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/bibica.net.crt -O /root/ssl/bibica.net.crt
-# setup ssl for bibica.net and api.bibica.net
+# setup ssl for bibica.net
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/bibica.net -O /etc/nginx/sites-available/bibica.net
-sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/api.bibica.net -O //etc/nginx/sites-available/api.bibica.net
 # nginx reload
 nginx -t
 sudo service nginx reload
-# mariadb config
-sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/my.cnf -O /etc/mysql/my.cnf
-sudo service mysql restart
-# delete all file in foder htdocs
+# go to root bibica.net
 cd /var/www/bibica.net/htdocs
-rm -rf *
-
