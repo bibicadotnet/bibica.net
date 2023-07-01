d09db06b3ebb80898ed33e76074c8e8ee1247494
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-07-01 21:00:27 +0700

    Update setup_oracle_24gb.sh

--- setup_oracle_24gb.sh
+++ setup_oracle_24gb.sh
@@ -53,15 +53,12 @@ sudo service nginx reload
 
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/php_vm_standard_a1_flex.ini -O /etc/php/7.4/fpm/php.ini
 sudo service php7.4-fpm restart
-sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Webinoly-Optimization/master/my_vm_standard_a1_flex.cnf -O /etc/mysql/my.cnf
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/my_vm_standard_a1_flex.cnf -O /etc/mysql/my.cnf
 sudo service mysql restart
 
 sudo apt update && sudo apt upgrade -y
 sudo webinoly -verify
 sudo webinoly -info
-
-
-
 
 cd /var/www/bibica.net/htdocs
 rm -rf *
