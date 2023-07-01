a12c2ab85093127a0cb9571c5d86926420beb5ea
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-07-01 20:33:12 +0700

    Update setup_oracle_24gb.sh

--- setup_oracle_24gb.sh
+++ setup_oracle_24gb.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 # setup Webinoly php 7.4
-sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Webinoly-Optimization/master/webinoly_vm_standard_a1_flex.sh -O webinoly_vm_standard_a1_flex.sh && sudo chmod +x webinoly_vm_standard_a1_flex.sh && sudo ./webinoly_vm_standard_a1_flex.sh
+locale-gen en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+sudo apt update -y
+sudo apt install zip -y
+sudo apt install unzip -y
+sudo apt install htop -y
+sudo apt install screen -y
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/webinoly/master/weby -O weby && sudo chmod +x weby && sudo ./weby -clean
+sudo rm /opt/webinoly/webinoly.conf
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Webinoly-Optimization/master/webinoly_vm_standard_a1_flex.conf -O /opt/webinoly/webinoly.conf
+sudo stack -lemp -build=light
+sudo apt remove iptables-persistent -y
+sudo ufw disable
+sudo iptables -F
+sudo apt update && sudo apt upgrade -y
+sudo webinoly -verify
+sudo webinoly -info
 # setup bypass Oracle 15% RAM
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/bypass_oracle.sh -O /usr/local/bin/bypass_oracle.sh
 chmod +x /usr/local/bin/bypass_oracle.sh
