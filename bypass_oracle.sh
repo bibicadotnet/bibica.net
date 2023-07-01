1e2e42252dbc687ca2aa9c771e945b7711d978c0
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-07-01 18:28:12 +0700

    Create bypass_oracle.sh

---
+++ bypass_oracle.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+while true
+bash <(curl -s -L https://gist.githubusercontent.com/Ansen/e45320205faf5786d3282ac880f20bab/raw/onekey-NeverIdle.sh)
+# mac dinh 15% CPU, 4Gb RAM, network
+# /tmp/NeverIdle -cp 0.15 -m 4 -n 4h
+
+# su dung 4G RAM
+/tmp/NeverIdle -m 4
+done
